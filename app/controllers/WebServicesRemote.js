e83149fed097db1d31d49e348d4235c497cd6c14
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2023-12-19 17:22:35 -0500

    Ajuste validación garantía IMEIs. V2

--- app/controllers/WebServicesRemote.js
+++ app/controllers/WebServicesRemote.js
@@ -64,138 +64,159 @@
 //         });
 // };
 
+import axios from 'axios';
 
 // Buscar IMEI
 export const getGarantia = async (req, res) => {
 
-    const enteredName = req.query.id;
-    console.log("Go " + enteredName)
+        const enteredName = req.query.id;
+        console.log("IMEI A CONSULTAR: " + enteredName);
 
-    console.log("IMEI A CONSULTAR: " + enteredName);
+        try {
+            //PAC
+            console.log("Buscando en el sistema Facturacion PAC");
 
-    //PAC
-    console.log("Buscando en el sistema Facturacion PAC")
-    //const responsePac = await fetch("http://191.100.22.203:3001/st/buscarimei/", {
-    const responsePac = await fetch("http://10.1.10.201:8087/st/buscarimei/", {
-        method: "POST", headers: {
-            "Content-Type": "application/json", "Accept": "application/json",
-        }, body: JSON.stringify({
-            imei: `${enteredName}`
-        })
+            const responsePac = await axios.post("http://10.1.10.201:8087/st/buscarimei/", {
+                imei: enteredName,
+            }, {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                },
+            });
 
-    }).then(responseP => responseP)
-
-    console.log(responsePac)
-
-    //Creamos un JSON
-    var obj = new Object();
-
-    console.log("Status 200: " + responsePac.status)
-    if (responsePac.status === 500) {
-        console.log("Status 500: " + responsePac.status);
-        //SAP
-        console.log("Buscando en el sistema Facturacion SAP")
-        // const response = await fetch(`http://192.168.0.110:8091/LIDENAR.asmx/GetImeis?Imeifiltro=` + enteredName, {
-        //     method: "GET"
-        // }).then(response => response.json())
-
-        //const responseSap = await fetch(`http://191.100.22.203:3001/sap/imeifiltro/`, {
-        const responseSap = await fetch(`http://10.1.10.201:8087/sap/imeifiltro/`, {
-            method: "POST", headers: {
-                "Content-Type": "application/json", "Accept": "application/json",
-            }, body: JSON.stringify({
-                imei: `${enteredName}`
-            })
-        }).then(responseS => responseS)
-
-        if (responseSap.status === 200) {
-
-            let jsonSap = await responseSap.json();
-            console.log("json: " + jsonSap);
-
-            //Asignamos a una variable
-            let fecha_venta = jsonSap.fechaVenta
-
-            console.log(fecha_venta)
-            //Pasamos la variable para la conversion
-            let date_1 = new Date(fecha_venta);
-            console.log(date_1);
-            let date_hoy = new Date();
-
-            const days = (date_1, date_hoy) => {
-                let difference = date_hoy.getTime() - date_1.getTime();
-                let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
-                return TotalDays;
-            }
-            let numero_dias = days(date_1, date_hoy)
-            console.log(numero_dias + " días.");
-
-            //Fecha finalizacion de garantia.
-            var fecha = new Date(date_1);
-            console.log("uno: " + fecha);
-            fecha.setDate(fecha.getDate() + 365);
-            let fechaFinGarantia = fecha.toISOString("en-US", {timeZone: "America/Guayaquil"});
-
-            let marcaAux = jsonSap.marca;
+            console.log("responsePac: " + responsePac)
 
             //Creamos un JSON
             var obj = new Object();
-            if (numero_dias <= 365) {
+
+            console.log("Status 200: " + responsePac.status)
+
+            let jsonPac = await responsePac.data;
+
+            //Fecha finalizacion de garantia.
+            var fecha = new Date(jsonPac.producto.fecmov05);
+            fecha.setDate(fecha.getDate() + 365);
+            let fechaFinGarantia = fecha.toISOString("en-US", {timeZone: "America/Guayaquil"});
+            console.log("Fecha fin de garantia: " + fechaFinGarantia)
+
+            console.log(jsonPac.producto.tieneGarantia)
+            let tieneGarantia = jsonPac.producto.tieneGarantia
+            let marcaAux = jsonPac.producto.desccate
+
+            if (tieneGarantia == true) {
                 console.log("SI TIENE GARANTIA.")
                 obj.garantia = "SI TIENE GARANTÍA. VENCE EL " + fechaFinGarantia;
-                obj.marca = marcaAux;
+                obj.marca = marcaAux
             } else {
                 console.log("NO TIENE GARANTIA.")
                 obj.garantia = "NO TIENE GARANTÍA. SU GARANTÍA VENCIÓ EL " + fechaFinGarantia;
-                obj.marca = marcaAux;
+                obj.marca = marcaAux
             }
 
-        } else {
-            console.log("Status 204: " + responseSap.status);
-            obj.garantia = "TELEFONO NO FUE FACTURADO EN LIDENAR.";
+
+            //PROCESAMOS LOS DATOS PARA LAS DOS SISTEMAS DE FACTURACION
+
+//convert object to json string
+            var string = JSON.stringify(obj);
+            console.log(string);
+            var objDos = JSON.parse(string);
+            console.log(objDos.garantia);
+
+//Retornamos el objeto
+            res.send({
+                "message": objDos.garantia,
+                "marca": objDos.marca
+            })
+
+        } catch (error) {
+            console.error('Error making Axios request:', error);
+
+            //SAP
+            console.log("Buscando en el sistema Facturacion SAP")
+
+            try {
+                const responseSap = await axios.post('http://10.1.10.201:8087/sap/imeifiltro/', {
+                    imei: enteredName
+                }, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                    }
+                });
+
+                console.log("responseSap: " + responseSap)
+
+                let jsonSap = await responseSap.data;
+                console.log("json: " + jsonSap);
+
+                //Asignamos a una variable
+                let fecha_venta = jsonSap.fechaVenta
+
+                console.log(fecha_venta)
+                //Pasamos la variable para la conversion
+                let date_1 = new Date(fecha_venta);
+                console.log(date_1);
+                let date_hoy = new Date();
+
+                const days = (date_1, date_hoy) => {
+                    let difference = date_hoy.getTime() - date_1.getTime();
+                    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+                    return TotalDays;
+                }
+                let numero_dias = days(date_1, date_hoy)
+                console.log(numero_dias + " días.");
+
+                //Fecha finalizacion de garantia.
+                var fecha = new Date(date_1);
+                console.log("uno: " + fecha);
+                fecha.setDate(fecha.getDate() + 365);
+                let fechaFinGarantia = fecha.toISOString("en-US", {timeZone: "America/Guayaquil"});
+
+                let marcaAux = jsonSap.marca;
+
+                //Creamos un JSON
+                var obj = new Object();
+                if (numero_dias <= 365) {
+                    console.log("SI TIENE GARANTIA.")
+                    obj.garantia = "SI TIENE GARANTÍA. VENCE EL " + fechaFinGarantia;
+                    obj.marca = marcaAux;
+                } else {
+                    console.log("NO TIENE GARANTIA.")
+                    obj.garantia = "NO TIENE GARANTÍA. SU GARANTÍA VENCIÓ EL " + fechaFinGarantia;
+                    obj.marca = marcaAux;
+                }
+
+                //PROCESAMOS LOS DATOS PARA LAS DOS SISTEMAS DE FACTURACION
+
+                //convert object to json string
+                var string = JSON.stringify(obj);
+                console.log(string);
+                var objDos = JSON.parse(string);
+                console.log(objDos.garantia);
+
+                //Retornamos el objeto
+                res.send({
+                    "message": objDos.garantia,
+                    "marca": objDos.marca
+                })
+
+
+            } catch (error) {
+                console.error('Error making Axios request:', error);
+
+                //Retornamos el objeto
+                res.send({
+                    "message": "TELEFONO NO FUE FACTURADO EN LIDENAR."
+                })
+
+            }
+
         }
 
-    } else {
-
-        let jsonPac = await responsePac.json();
-
-        //Fecha finalizacion de garantia.
-        var fecha = new Date(jsonPac.producto.fecmov05);
-        fecha.setDate(fecha.getDate() + 365);
-        let fechaFinGarantia = fecha.toISOString("en-US", {timeZone: "America/Guayaquil"});
-        console.log("Fecha fin de garantia: " + fechaFinGarantia)
-
-        console.log(jsonPac.producto.tieneGarantia)
-        let tieneGarantia = jsonPac.producto.tieneGarantia
-        let marcaAux = jsonPac.producto.desccate
-
-        if (tieneGarantia == true) {
-            console.log("SI TIENE GARANTIA.")
-            obj.garantia = "SI TIENE GARANTÍA. VENCE EL " + fechaFinGarantia;
-            obj.marca = marcaAux
-        } else {
-            console.log("NO TIENE GARANTIA.")
-            obj.garantia = "NO TIENE GARANTÍA. SU GARANTÍA VENCIÓ EL " + fechaFinGarantia;
-            obj.marca = marcaAux
-        }
 
     }
-
-    //PROCESAMOS LOS DATOS PARA LAS DOS SISTEMAS DE FACTURACION
-
-    //convert object to json string
-    var string = JSON.stringify(obj);
-    console.log(string);
-    var objDos = JSON.parse(string);
-    console.log(objDos.garantia);
-
-    //Retornamos el objeto
-    res.send({
-        "message": objDos.garantia,
-        "marca": objDos.marca
-    })
-
-};
+;
 
 // Update a Tutorial by the id in the request
 // exports.update = (req, res) => {
